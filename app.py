553b36e6aa5f8fb8a8f7ff6b6db3adc57d343601
gkm05 <er.gkm05@gmail.com>
2024-12-16 10:06:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain.agents import initialize_agent, AgentType
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
-  
+####
 ##Used the in built tool of wikipedia
 api_wrapper_wiki = WikipediaAPIWrapper( top_k_results=1, doc_content_chars_max=250)
 wiki = WikipediaQueryRun(api_wrapper = api_wrapper_wiki)
